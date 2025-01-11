bdb224c02f253e9e40d8e96bc801bc65e6664d13
silent-cipher <puspendramahariya@gmail.com>
2025-01-11 11:15:23 +0530

    use suspense

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,12 +3,22 @@ import { useAccount } from "wagmi";
 import { useSearchParams } from "next/navigation";
 import styles from "~~/styles/admin.module.css";
 import Button from "~~/components/ui/Button";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { CreatePollForm, PollsList } from "~~/components/admin";
 import { useFetchUserPolls } from "~~/hooks/useFetchUserPolls";
 import { Pagination } from "~~/components/home";
 
 export default function Admin() {
+  return (
+    <div className={styles.wrapper}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AdminPoll />
+      </Suspense>
+    </div>
+  );
+}
+
+const AdminPoll = () => {
   const searchParams = useSearchParams();
   const [showCreatePoll, setShowCreatePoll] = useState(false);
   const { address, isConnected } = useAccount();
@@ -33,61 +43,53 @@ export default function Admin() {
 
   if (error) {
     return (
-      <div className={styles.wrapper}>
-        <div className={styles["admin-page"]}>
-          <div className={styles["error-state"]}>
-            <h3>Something went wrong</h3>
-            <p>Failed to load polls. Please try again later.</p>
-            <Button className={styles["retry-btn"]} action={refetchPolls}>
-              Retry
-            </Button>
-          </div>
+      <div className={styles["admin-page"]}>
+        <div className={styles["error-state"]}>
+          <h3>Something went wrong</h3>
+          <p>Failed to load polls. Please try again later.</p>
+          <Button className={styles["retry-btn"]} action={refetchPolls}>
+            Retry
+          </Button>
         </div>
       </div>
     );
   }
-
   return (
-    <div className={styles.wrapper}>
-      <div className={styles["admin-page"]}>
-        <div className={styles.header}>
-          {!showCreatePoll && isConnected && (
-            <Button
-              className={styles.btn}
-              action={() => setShowCreatePoll(true)}
-            >
-              Create Poll
-            </Button>
-          )}
-        </div>
-        {!showCreatePoll ? (
-          <>
-            <PollsList
-              polls={polls}
-              isLoading={isLoading}
-              refetch={refetchPolls}
-              error={error}
-            />
-            {polls && polls.length > 0 && (
-              <div className={styles["pagination-container"]}>
-                <Pagination
-                  currentPage={currentPage}
-                  setCurrentPage={setCurrentPage}
-                  totalItems={totalPolls}
-                  itemsPerPage={limit}
-                />
-              </div>
-            )}
-          </>
-        ) : (
-          <CreatePollForm
-            refetchPolls={refetchPolls}
-            onClose={() => {
-              setShowCreatePoll(false);
-            }}
-          />
+    <div className={styles["admin-page"]}>
+      <div className={styles.header}>
+        {!showCreatePoll && isConnected && (
+          <Button className={styles.btn} action={() => setShowCreatePoll(true)}>
+            Create Poll
+          </Button>
         )}
       </div>
+      {!showCreatePoll ? (
+        <>
+          <PollsList
+            polls={polls}
+            isLoading={isLoading}
+            refetch={refetchPolls}
+            error={error}
+          />
+          {polls && polls.length > 0 && (
+            <div className={styles["pagination-container"]}>
+              <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                totalItems={totalPolls}
+                itemsPerPage={limit}
+              />
+            </div>
+          )}
+        </>
+      ) : (
+        <CreatePollForm
+          refetchPolls={refetchPolls}
+          onClose={() => {
+            setShowCreatePoll(false);
+          }}
+        />
+      )}
     </div>
   );
-}
+};
